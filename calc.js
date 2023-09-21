1349150e9479eb0b95cb1baa0e8c2513fed34511
camilee-olivr <camile.franca@etec.sp.gov.br>
2023-09-21 11:36:24 -0300

    atualizacao

--- calc.js
+++ calc.js
@@ -12,3 +12,17 @@ function s(){
     let res = document.getElementById('resultado')
     res.innerHTML = `<p> O resultado da soma é ${n3}.<p>`
 }
+function m(){
+    let n1 = Number(window.prompt("Digite o primeiro número:"))
+    let n2 = Number(window.prompt("Digite o segundo número:"))
+    n3 = n1*n2
+    let res = document.getElementById('resultado')
+    res.innerHTML = `<p> O resultado da soma é ${n3}.<p>`
+}
+function d(){
+    let n1 = Number(window.prompt("Digite o primeiro número:"))
+    let n2 = Number(window.prompt("Digite o segundo número:"))
+    n3 = n1/n2
+    let res = document.getElementById('resultado')
+    res.innerHTML = `<p> O resultado da soma é ${n3}.<p>`
+}
